6b1df56134ce69e4cd6fd1c4b80707aa5738609b
vs-cgo <victorshavlikov@gmail.com>
2025-02-07 14:26:58 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"golang.org/x/image/bmp"
+	"image"
+	"image/color"
+	"image/draw"
 	"log"
 	"net/http"
-  "time"  
-  "sync"
-  "encoding/json"
+	"sync"
+	"time"
 
-  "fyne.io/fyne/v2"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	_"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/canvas"
+	_ "fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -23,8 +28,8 @@ type User struct {
 }
 
 type Data struct {
-	Info [][]string
-  Mutex sync.Mutex
+	Info  [][]string
+	Mutex sync.Mutex
 }
 
 var data Data
@@ -32,70 +37,74 @@ var data Data
 func main() {
 	a := app.New()
 	w := a.NewWindow("Server")
-
+	w.SetOnClosed(func() {
+		a.Quit()
+	})
 	server := http.Server{Addr: ":8888", Handler: nil}
 	go startServer(&server)
-  settingWindow(&w)
-  /*
-	//
-	buttonHandler := func(row int) {
-		log.Printf("Нажата кнопка в строке %d\n", row+1)
-	}
-*/
-  
-/*
-	table.OnSelected = func(id string) {
-		rowIndex, _ := strconv.Atoi(id)
-		if rowIndex >= 0 && rowIndex < len(data) {
-			buttonHandler(rowIndex)
-		}
-	}
-*/
+	settingWindow(&w, a)
+
 	w.ShowAndRun()
 	server.Close()
 }
 
-func settingWindow(w *fyne.Window) {  
- table := widget.NewTableWithHeaders(
-          func() (int, int) {
-            return len(data.Info), 5
-          },
-          func() fyne.CanvasObject {
-            return widget.NewLabel("test")
-          },
-          func(i widget.TableCellID, o fyne.CanvasObject) {
-            o.(*widget.Label).SetText(data.Info[i.Row][i.Col])
-          },
-  )
-  table.ShowHeaderColumn = false
-  table.CreateHeader = func() fyne.CanvasObject {
-    return widget.NewLabel("Header")
-  }
+func settingWindow(w *fyne.Window, a fyne.App) {
+	table := widget.NewTableWithHeaders(
+		func() (int, int) {
+			return len(data.Info), 5
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("test")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data.Info[i.Row][i.Col])
+		},
+	)
 
-  table.UpdateHeader = func(id widget.TableCellID, template fyne.CanvasObject) {
-    switch id.Col {
-    case 0:
-      template.(*widget.Label).SetText("Domain")
-    case 1:
-      template.(*widget.Label).SetText("Mashine")
-    case 2:
-      template.(*widget.Label).SetText("Ip")
-    case 3:
-      template.(*widget.Label).SetText("User")
-    case 4:  
-      template.(*widget.Label).SetText("Last")
-    }
-  
-  }
+	table.OnSelected = func(id widget.TableCellID) {
+		if id.Col == 4 {
+			data.Mutex.Lock()
+			screenSave(a, data.Info[id.Row][2])
+			data.Mutex.Unlock()
+		}
+		table.Unselect(id)
+	}
 
-  table.SetColumnWidth(0, 100)
-  table.SetColumnWidth(1, 150)
-  table.SetColumnWidth(2, 170)
-  table.SetColumnWidth(3, 130)
-  table.SetColumnWidth(4, 150)
-  go update(table)
-  (*w).SetContent(table)
-  (*w).Resize(fyne.Size{780, 400})
+	table.ShowHeaderColumn = false
+	table.CreateHeader = func() fyne.CanvasObject {
+		return widget.NewLabel("Header")
+	}
+
+	table.UpdateHeader = func(id widget.TableCellID, template fyne.CanvasObject) {
+		switch id.Col {
+		case 0:
+			template.(*widget.Label).SetText("Domain")
+		case 1:
+			template.(*widget.Label).SetText("Mashine")
+		case 2:
+			template.(*widget.Label).SetText("Ip")
+		case 3:
+			template.(*widget.Label).SetText("User")
+		case 4:
+			template.(*widget.Label).SetText("Last")
+		}
+	}
+
+	table.SetColumnWidth(0, 100)
+	table.SetColumnWidth(1, 150)
+	table.SetColumnWidth(2, 170)
+	table.SetColumnWidth(3, 130)
+	table.SetColumnWidth(4, 150)
+	go update(table)
+	(*w).SetContent(table)
+	(*w).Resize(fyne.Size{780, 400})
+}
+
+func screenSave(a fyne.App, url string) {
+	nw := a.NewWindow("Image")
+	img := getImage(url)
+	nw.SetContent(img)
+	nw.Show()
 }
 
 func startServer(s *http.Server) {
@@ -105,44 +114,73 @@ func startServer(s *http.Server) {
 }
 
 func addUserHandler(w http.ResponseWriter, r *http.Request) {
-  if r.Method != "POST" {
-    http.Error(w, "wrong method", http.StatusMethodNotAllowed)
-    return
-  }
-  var u User
-  err := json.NewDecoder(r.Body).Decode(&u)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
+	if r.Method != "POST" {
+		http.Error(w, "wrong method", http.StatusMethodNotAllowed)
+		return
+	}
+	var u User
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  data.Mutex.Lock()
-  data.Info = append(data.Info, 
-  []string{u.Domain, u.Mashine, u.IP, u.User, time.Now().Format("02.01.2006 15:04:05")})
-  data.Mutex.Unlock()
+	data.Mutex.Lock()
+	data.Info = append(data.Info,
+		[]string{u.Domain, u.Mashine, u.IP, u.User, time.Now().Format("02.01.2006 15:04:05")})
+	data.Mutex.Unlock()
 
-  w.WriteHeader(http.StatusOK)
-  w.Write([]byte("200 OK"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("200 OK"))
 }
 
 func update(t *widget.Table) {
-  for {
-    for i := 0; i < len(data.Info); i++ {
-      if ping(data.Info[i][2]) {
-        data.Info[i][4] = time.Now().Format("02.01.2006 15:04:05")
-      }
-    }
-
-    t.Refresh()
-    time.Sleep(10 * time.Second)
-  }  
+	for {
+		for i := 0; i < len(data.Info); i++ {
+			if ping(data.Info[i][2]) {
+				data.Info[i][4] = time.Now().Format("02.01.2006 15:04:05")
+			}
+		}
+		t.Refresh()
+		time.Sleep(10 * time.Second)
+	}
 }
 
 func ping(url string) bool {
-  str := fmt.Sprintf("http://%s:7777/ping", url)
-  res, err := http.Get(str) 
-  if err != nil || res.StatusCode != http.StatusOK {
-    return false
-  }
-  return true
+	str := fmt.Sprintf("http://%s:7777/ping", url)
+	res, err := http.Get(str)
+	if err != nil || res.StatusCode != http.StatusOK {
+		return false
+	}
+	return true
+}
+
+func getImage(url string) *canvas.Image {
+	str := fmt.Sprintf("http://%s:7777/getimage", url)
+	res, err := (&(http.Client{Timeout: 1 * time.Second})).Get(str)
+	img := image.NewRGBA(image.Rect(0, 0, 300, 300))
+
+	if err != nil || res.StatusCode != http.StatusOK {
+		draw.Draw(img, img.Bounds(),
+			&image.Uniform{color.RGBA{R: 250, G: 100, B: 100, A: 255}},
+			image.ZP, draw.Src)
+		fimg := canvas.NewImageFromImage(img)
+		fimg.SetMinSize(fyne.NewSize(300, 300))
+		return fimg
+	}
+	ret, err := bmp.Decode(res.Body)
+	if err != nil {
+		draw.Draw(img, img.Bounds(),
+			&image.Uniform{color.RGBA{R: 100, G: 250, B: 100, A: 255}},
+			image.ZP, draw.Src)
+		fimg := canvas.NewImageFromImage(img)
+		fimg.SetMinSize(fyne.NewSize(300, 300))
+		return fimg
+	}
+	defer res.Body.Close()
+
+	fimg := canvas.NewImageFromImage(ret)
+	fimg.SetMinSize(fyne.NewSize(600, 600))
+	fimg.FillMode = canvas.ImageFillOriginal
+	return fimg
 }
